5d5fd916dfae4bb825b4ab60f8e8d96d939318ab
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-07-21 13:32:38 +0700

    Connection to Database

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php 
+    // connect to database
+    $conn =  mysqli_connect('localhost', 'root', '', 'pizza_town');
+
+    // check connection
+    if( !$conn ) {
+        echo 'Connection  error : ' . mysqli_connect_error();
+    }
+
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY create_at';
+
+    // make query & get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result form memory
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
