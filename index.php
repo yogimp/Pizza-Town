154030fddab4f5723692b5b26eeb9d820ec39709
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-07-21 13:55:54 +0700

    getting data from database

--- index.php
+++ index.php
@@ -22,14 +22,40 @@
     // close connection
     mysqli_close($conn);
 
-    print_r($pizzas);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include('templates/header.php') ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+    <div class="row">
+
+        <?php foreach( $pizzas as $pizza ) : ?>
+
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?= htmlspecialchars($pizza['title']); ?></h6>
+                        <ul>
+                            <?php foreach( explode(',', $pizza['ingredients']) as $ing ) : ?>
+                                <li><?= htmlspecialchars($ing) ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+
+    </div>
+</div>
 
 <?php include('templates/footer.php') ?>
 
